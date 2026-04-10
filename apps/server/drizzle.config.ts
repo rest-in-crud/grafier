af32c000224380f8d9e812f8359e9155c71bbcf9
RofaBR <rostislavbrigine@gmail.com>
2026-04-10 20:55:24 +0300

    Feat/setup (#3)
    * chore: initial setup with `turborepo` and stuff
    
    * chore: set up cors and npm vulnerabilities be gone
    
    * ci: continuous integration pipeline with linting and audit
    
    * fix: linting and a couple of configs
    
    * fix: have it your way
    
    * chore: move from `TypeORM` to `Drizzle`
    
    * ci: bring audit level to high
    
    * chore: update Dockerfiles and dependencies
    
    ---------
    
    Co-authored-by: ovalkonia <ovalkonia@gmail.com>

---
+++ apps/server/drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '../../.env') });
+
+export default defineConfig({
+  schema: './src/database/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+  },
+});
