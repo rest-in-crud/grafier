d4c2af170dde42d8686353aa94259ff0a1f96896
RofaBR <rostislavbrigine@gmail.com>
2026-04-14 16:24:03 +0300

    refactor(auth): remove manual auth implementation

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -12,7 +11,6 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
     }),
     DatabaseModule,
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
