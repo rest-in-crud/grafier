5ac5e0b9ce3b56d77f8fa2d9fa8e94e20665b2f9
RofaBR <rostislavbrigine@gmail.com>
2026-04-17 14:55:01 +0300

    chore(infra): configure automated database migrations
    - Add database migration execution script (migrate.ts)
    - Update Dockerfile to support migration execution
    - Add independent migrate service to compose.dev.yaml and compose.prod.yaml
    - Update CI workflow to support new Docker configuration
    - Update main.ts entry point

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,32 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { Pool } from 'pg';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const config = app.get(ConfigService);
-
-    const pool = new Pool({
-        host: config.getOrThrow('DB_HOST'),
-        port: config.getOrThrow<number>('DB_PORT'),
-        user: config.getOrThrow('DB_USER'),
-        password: config.getOrThrow('DB_PASSWORD'),
-        database: config.getOrThrow('DB_NAME'),
-    });
-    await migrate(drizzle(pool), {
-        migrationsFolder: `${process.cwd()}/drizzle`,
-    });
-    await pool.end();
-
     const host = config.getOrThrow<string>('URL_BACKEND');
     const port = config.getOrThrow<number>('SERVER_PORT');
 
     app.use(cookieParser());
-
     app.enableCors({
         origin: config.getOrThrow<string>('URL_FRONTEND'),
         credentials: true,
