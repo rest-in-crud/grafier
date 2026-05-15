8bdef5d7ab95c453bfe6ba13ac8ed23d688276f3
ovalkonia <ovalkonia@gmail.com>
2026-05-15 21:07:12 +0300

    chore: another lint thingy

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,9 +1,4 @@
-import {
-    ConflictException,
-    Inject,
-    Injectable,
-    UnauthorizedException,
-} from '@nestjs/common';
+import { ConflictException, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request, Response } from 'express';
