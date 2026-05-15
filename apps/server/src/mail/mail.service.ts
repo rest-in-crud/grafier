c292ba34d04929fdf41f7884834f96d75669a721
ovalkonia <ovalkonia@gmail.com>
2026-05-16 00:40:26 +0300

    feat: add appropriate mail service templates and methods for email verification

--- apps/server/src/mail/mail.service.ts
+++ apps/server/src/mail/mail.service.ts
@@ -4,6 +4,7 @@ import { render } from 'react-email';
 import * as React from 'react';
 import { MAIL_PROVIDER, MailProvider } from './interfaces/mail-provider.interface';
 import { ForgotPasswordTemplate } from './templates/forgot-password.template';
+import { VerifyEmailTemplate } from './templates/verify-email.template';
 
 @Injectable()
 export class MailService {
@@ -26,6 +27,24 @@ export class MailService {
         await this.provider.send({
             to: email,
             subject: 'Reset your password',
+            html,
+        });
+    }
+
+    async sendVerificationEmail(email: string, name: string, token: string) {
+        const frontendUrl = this.config.getOrThrow<string>('URL_FRONTEND').replace(/\/$/, '');
+        const verifyLink = `${frontendUrl}/verify/${token}`;
+
+        const html = await render(
+            React.createElement(VerifyEmailTemplate, {
+                name,
+                verifyLink,
+            }),
+        );
+
+        await this.provider.send({
+            to: email,
+            subject: 'Verify your email address',
             html,
         });
     }
