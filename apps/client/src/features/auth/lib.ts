c696b72bbb31f7d03794ee20375a4352b6d1210f
niykuta <niykuta@gmail.com>
2026-05-22 00:22:01 +0200

    chore(auth): remove dead Zustand auth store and lib helpers

--- apps/client/src/features/auth/lib.ts
+++
@@ -1,9 +0,0 @@
-import { clearAccessToken } from '@/features/auth/token';
-import { useAuthStore } from '@/features/auth/store';
-
-const clearAuth = () => {
-  clearAccessToken();
-  useAuthStore.getState().clearUser();
-};
-
-export { clearAuth };
