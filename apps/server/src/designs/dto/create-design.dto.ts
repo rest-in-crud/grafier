ee2e24393cb40438e3f0270d2017bff6e9b1e6de
ovalkonia <ovalkonia@gmail.com>
2026-05-24 01:11:33 +0300

    feat: update and rebrand the dtos

---
+++ apps/server/src/designs/dto/create-design.dto.ts
@@ -0,0 +1,25 @@
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive, IsString, MaxLength } from 'class-validator';
+
+export class CreateDesignDto {
+    @IsString()
+    @MaxLength(255)
+    name!: string;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    width?: number;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    height?: number;
+
+    @IsOptional()
+    @IsBoolean()
+    isPublic?: boolean;
+
+    @IsOptional()
+    @IsEnum(['project', 'template'])
+    type?: 'project' | 'template';
+}
---
+++ apps/server/src/designs/dto/update-design.dto.ts
@@ -0,0 +1,26 @@
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive, IsString, MaxLength } from 'class-validator';
+
+export class UpdateDesignDto {
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    name?: string;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    width?: number;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    height?: number;
+
+    @IsOptional()
+    @IsBoolean()
+    isPublic?: boolean;
+
+    @IsOptional()
+    @IsEnum(['project', 'template'])
+    type?: 'project' | 'template';
+}
