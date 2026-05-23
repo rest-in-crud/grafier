438f4796c8dc6e4004b261af84831d6b1b97eb1d
Mykola Kardash <60359793+ovalkonia@users.noreply.github.com>
2026-05-23 16:28:59 -0700

    feat: project routes rebranding (#126)
    * feat: add an optional jwt auth guard
    
    * chore: rebrand schema with `projects` into `designs` and add two new fields
    
    * feat: update and rebrand the dtos
    
    * feat: update and rebrand the design service
    
    * feat: design controllers and module
    
    * feat: add design routes for users controller
    
    * chore: cleanup
    
    * chore: run format
    
    * fix: rewrite the optional auth guard without `any`
    
    * feat: include username and remove own designs for `GET` design routes
    
    * chore: run format

---
+++ apps/server/src/designs/designs.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { DesignsService } from './designs.service';
+import { DesignsController } from './designs.controller';
+import { ProjectsAliasController } from './projects-alias.controller';
+import { TemplatesAliasController } from './templates-alias.controller';
+
+@Module({
+    controllers: [DesignsController, ProjectsAliasController, TemplatesAliasController],
+    providers: [DesignsService],
+    exports: [DesignsService],
+})
+export class DesignsModule {}
---
+++ apps/server/src/designs/templates-alias.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwtAuth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { AuthUser } from '../types/auth.types';
+import { DesignsService } from './designs.service';
+
+@Controller('templates')
+export class TemplatesAliasController {
+    constructor(private readonly designsService: DesignsService) {}
+
+    @Get()
+    list() {
+        return this.designsService.listDesigns('template');
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post(':id/fork')
+    fork(@CurrentUser() user: AuthUser, @Param('id') id: string) {
+        return this.designsService.forkDesign(id, user.id, 'template');
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post(':id/copy')
+    copy(@CurrentUser() user: AuthUser, @Param('id') id: string) {
+        return this.designsService.copyDesign(id, user.id, 'template');
+    }
+}
