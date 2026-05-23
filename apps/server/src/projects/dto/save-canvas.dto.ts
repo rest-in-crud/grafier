3e04ead989dc9b39da02c996ee154a9b1f812d3f
ovalkonia <ovalkonia@gmail.com>
2026-05-24 01:13:49 +0300

    chore: cleanup

--- apps/server/src/projects/dto/save-canvas.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsArray, IsObject } from 'class-validator';
-
-export class SaveCanvasDto {
-    @IsObject()
-    canvasJSON!: Record<string, unknown>;
-
-    @IsArray()
-    layersJSON!: unknown[];
-}
